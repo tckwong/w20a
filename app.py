10aa6d161aadb51f7cb944defc1642c7edb8f47c
Trevor <wtrevor242@gmail.com>
2021-09-26 17:54:04 -0600

    bonus completed

--- app.py
+++ app.py
@@ -4,6 +4,38 @@ import mariadb
 conn = None
 cursor = None
 
+def addContent():
+        print("Please type content")
+        usr_content_input = input()
+        try:
+            cursor.execute("INSERT INTO blog_post(username, content) VALUES(?, ?)", [username_input, usr_content_input])
+        except mariadb.DataError:
+            print("Something wrong with your data")
+        except mariadb.OperationalError:
+            print("Operational error on query")
+        if(cursor.rowcount == 1):
+            print("Update sucessful")
+            conn.commit()
+        else:
+            print("Failed to update")
+
+def lookupUsers():
+    cursor.execute("SELECT content FROM blog_post")
+    post_list = cursor.fetchall()
+    print("There are ", cursor.rowcount, " users!")
+    for posts in post_list:
+        print(posts)
+
+def checkLoginCreds(usrname, password):
+    cursor.execute("SELECT username, password FROM users WHERE username=?",[usrname])
+    getUserInfo = cursor.fetchone()
+    #check inputs against database
+    if (getUserInfo[0] == usrname and getUserInfo[1] == password):
+        print("Login Successful")
+    else:
+        print("Incorrect username/password combination")
+    cursor.close()
+    conn.close()
 try:
     conn = mariadb.connect(
                         user=dbcreds.user,
@@ -14,13 +46,17 @@ try:
                         )
     cursor = conn.cursor()
 
+    #Run login code
+    user_input_name = input("Input username: ")
+    user_input_pw = input("Input password: ")
+    checkLoginCreds(user_input_name, user_input_pw)
+    
     tryAgain = True
     while True:
         print("Please type in your username:")
         username_input = input()
         if not username_input:
-            print("Error: Please input a valid username")
-            continue
+            raise ValueError
         else:
             break
 
@@ -29,31 +65,15 @@ try:
         userChoice = int(input())
         
         if (userChoice == 1):
-            print("Please type content")
-            usr_content_input = input()
-            try:
-                cursor.execute("INSERT INTO blog_post(username, content) VALUES(?, ?)", [username_input, usr_content_input])
-            except mariadb.DataError:
-                print("Something wrong with your data")
-            except mariadb.OperationalError:
-                print("Operational error on query")
-            if(cursor.rowcount == 1):
-                print("Update sucessful")
-                conn.commit()
-            else:
-                print("Failed to update")
+            addContent()
         elif (userChoice == 2):
-            cursor.execute("SELECT username FROM blog_post")
-            user_list = cursor.fetchall()
-            print("There are ", cursor.rowcount, " users!")
-            for usr in user_list:
-                print(usr)
+            lookupUsers()
         elif (userChoice == 3):
             break
         else:
             print("Error, please input either 1 or 2")
             continue
-    
+
 except mariadb.DataError:
     print("Something wrong with your data")
 except mariadb.OperationalError: #Creating already existing table falls under OperationalError
@@ -62,6 +82,8 @@ except mariadb.ProgrammingError:
     print("Your query was wrong")
 except mariadb.IntegrityError:
     print("Your query would have broken the database")
+except ValueError:
+    print("Please input a username")
 except:
     print("Something went wrong")
 
