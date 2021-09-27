d95c4f6d4d8dd7217940a1a0c79eafa8bd8f4efd
Trevor <wtrevor242@gmail.com>
2021-09-27 11:53:43 -0600

    final submission

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def addContent():
         print("Please type content")
         usr_content_input = input()
         try:
-            cursor.execute("INSERT INTO blog_post(username, content) VALUES(?, ?)", [username_input, usr_content_input])
+            cursor.execute("INSERT INTO blog_post(username, content) VALUES(?, ?)", [user_input_name, usr_content_input])
         except mariadb.DataError:
             print("Something wrong with your data")
         except mariadb.OperationalError:
@@ -22,7 +22,7 @@ def addContent():
 def lookupUsers():
     cursor.execute("SELECT content FROM blog_post")
     post_list = cursor.fetchall()
-    print("There are ", cursor.rowcount, " users!")
+    print("There are ", cursor.rowcount, " posts in total!")
     for posts in post_list:
         print(posts)
 
@@ -34,8 +34,8 @@ def checkLoginCreds(usrname, password):
         print("Login Successful")
     else:
         print("Incorrect username/password combination")
-    cursor.close()
-    conn.close()
+        cursor.close()
+        conn.close()
 try:
     conn = mariadb.connect(
                         user=dbcreds.user,
@@ -46,22 +46,20 @@ try:
                         )
     cursor = conn.cursor()
 
-    #Run login code
-    user_input_name = input("Input username: ")
-    user_input_pw = input("Input password: ")
-    checkLoginCreds(user_input_name, user_input_pw)
-    
     tryAgain = True
     while True:
-        print("Please type in your username:")
-        username_input = input()
-        if not username_input:
+        #Run login code
+        user_input_name = input("Input username: ")
+        user_input_pw = input("Input password: ")
+        checkLoginCreds(user_input_name, user_input_pw)
+        if not user_input_name or not user_input_pw:
             raise ValueError
         else:
             break
 
     while True:
-        print("You have 3 choices: \n1.Write a new post \n2.See all other posts \n3.Exit application")
+        print("--------------------------------------------------------------------------------")
+        print("You have 3 choices: \n1.Write a new post \n2.See all posts \n3.Exit application")
         userChoice = int(input())
         
         if (userChoice == 1):
@@ -71,7 +69,7 @@ try:
         elif (userChoice == 3):
             break
         else:
-            print("Error, please input either 1 or 2")
+            print("Error, please input either 1, 2, or 3")
             continue
 
 except mariadb.DataError:
